75f963541c8ec7510812382bd7cc485a263b6c93
Setornam <setornamnyame44@gmail.com>
2025-11-15 16:10:28 +0000

    new directory

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+n = str(input("What's the weather like today? sunny/rainy/cold:"))
+  
+if n == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+if n== "rainy":
+    print("Don't forget to your umbrella and a raincoat.")
+if n== "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommedations for that weather condition.")
